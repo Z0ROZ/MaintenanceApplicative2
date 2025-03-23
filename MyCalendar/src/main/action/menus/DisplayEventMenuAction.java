454b44f530ee43c30e1b0e604cf0d2363f8f77ee
Z0ROZ <129069832+Z0ROZ@users.noreply.github.com>
2025-03-23 16:06:12 +0100

    Implementation du menu des événements

--- MyCalendar/src/main/action/menus/DisplayEventMenuAction.java
+++ MyCalendar/src/main/action/menus/DisplayEventMenuAction.java
@@ -3,52 +3,44 @@ package action.menus;
 import action.Action;
 import action.ListAction;
 import action.display_events.DisplayEventAction;
-import action.events_types.PeriodiqueAction;
-import action.events_types.RendezVousAction;
-import action.events_types.ReunionAction;
+import action.display_events.DisplayEventDayAction;
+import action.display_events.DisplayEventMonthAction;
+import action.display_events.DisplayEventWeekAction;
 import calendar.CalendarManager;
-import user.UserManager;
 
 import java.util.Scanner;
-
 
 public class DisplayEventMenuAction implements Action {
     private final Scanner scanner;
     private final ListAction listAction;
-    private final UserManager userManager;
 
-    public DisplayEventMenuAction(Scanner scanner, CalendarManager calendarManager, UserManager userManager) {
+    public DisplayEventMenuAction(Scanner scanner, CalendarManager calendarManager) {
         this.scanner = scanner;
-        this.userManager = userManager;
         this.listAction = new ListAction();
 
-        this.listAction.addAction(new DisplayEventAction(calendarManager));
-        this.listAction.addAction(new RendezVousAction(calendarManager, scanner, userManager));
-        this.listAction.addAction(new ReunionAction(calendarManager, scanner, userManager));
-        this.listAction.addAction(new PeriodiqueAction(calendarManager, scanner, userManager));
-        // Déconnexion
+        listAction.addAction(new DisplayEventAction(calendarManager));
+        listAction.addAction(new DisplayEventMonthAction(calendarManager,scanner));
+        listAction.addAction(new DisplayEventWeekAction(calendarManager,scanner));
+        listAction.addAction(new DisplayEventDayAction(calendarManager,scanner));
     }
 
 
     @Override
     public void execute() {
-        while (userManager.isLoggedIn()) {
-            System.out.println("\nBonjour, " + userManager.getCurrentUser().getUsername());
-            System.out.println("=== Menu Gestionnaire d'Événements ===");
-            listAction.displayActions();
-            System.out.print("Votre choix : ");
+        System.out.println("\n=== Menu de visualisation d'Événements ===");
+        listAction.displayActions();
+        System.out.print("Votre choix : ");
 
-            try {
-                int choice = Integer.parseInt(scanner.nextLine());
-                listAction.execute(choice - 1);
-            } catch (NumberFormatException e) {
-                System.out.println("Veuillez entrer un nombre valide.");
-            }
+        try {
+            int choice = Integer.parseInt(scanner.nextLine());
+            listAction.execute(choice - 1);
+        } catch (NumberFormatException e) {
+            System.out.println("Veuillez entrer un nombre valide.");
         }
     }
 
     @Override
     public String getName() {
-        return "";
+        return "Voir les événements";
     }
 }
